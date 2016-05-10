93ae62d463b0e05c0b925a0a1c75d58757406c9c
piotrek29100 <wawrzyniakp@student.mini.pw.edu.pl>
2016-05-11 01:38:07 +0200

    Initial Commit

---
+++ Msi/Msi/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Msi.Annotations;
+
+namespace Msi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string question;
+        private double value;
+
+        public string Question
+        {
+            get { return question; }
+            set
+            {
+                question = value;
+                OnPropertyChanged(nameof(Question));
+            }
+        }
+
+        public double Value
+        {
+            get { return value; }
+            set
+            {
+                this.value = value;
+                OnPropertyChanged(nameof(Value));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Question = "Question";
+            Value = 0.4;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void NextButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
